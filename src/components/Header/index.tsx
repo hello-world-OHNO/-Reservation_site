d09c51c530df19b03b8400fe7a6bc4abaab6878c
hello-world-OHNO <daruma0248@gmail.com>
2024-03-15 20:47:55 +0900

    3/15まで

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Flex, Box } from "@chakra-ui/react";
+import { Flex, Box, Link } from "@chakra-ui/react";
 
 const Header = () => {
   return (
     // Headerを中央揃えにする
     <Flex justifyContent="space-between" alignItems="center" maxWidth="1244px" height="48px" margin="0 auto" marginBottom="5px">
       <Box display="flex" alignItems="center" padding="28px">
-        <a href='http://localhost:3000' style={{ fontWeight: 'bold' }}>HW RVS</a>
+        <Link href='http://localhost:3000' style={{ fontWeight: 'bold' }}>HW RVS</Link>
         <img src="/assets/images/rsv_logo.png"
           alt="logo" width="48px" height="33px" />
       </Box>
       <Box display="flex" alignItems="center" padding="28px">
-        <a href="http://localhost:3000" style={{ padding: '12px', textDecoration: "none", color: "black" }} >店舗一覧</a>
-        <a href="http://localhost:3000/contact" style={{ padding: '12px', textDecoration: "none", color: "black" }}>お問い合わせ</a>
-        <a href="http://localhost:3000/chat" style={{ padding: '12px', textDecoration: "none", color: "black" }}>chat</a>
-        <a href="http://localhost:3000/login" style={{ padding: '12px', textDecoration: "none", color: "black" }}>ログイン</a>
+        <Link href="http://localhost:3000" style={{ padding: '12px', textDecoration: "none", color: "black" }} >店舗一覧</Link>
+        <Link href="http://localhost:3000/contact" style={{ padding: '12px', textDecoration: "none", color: "black" }}>お問い合わせ</Link>
+        <Link href="http://localhost:3000/chat" style={{ padding: '12px', textDecoration: "none", color: "black" }}>chat</Link>
+        <Link href="http://localhost:3000/login" style={{ padding: '12px', textDecoration: "none", color: "black" }}>ログイン</Link>
       </Box>
     </Flex>
   );
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -20,8 +20,8 @@ const Login = () => {
               <h2 style={{ fontWeight: 'bold', fontSize: '36px', marginTop: "80px" }}>ログイン</h2>
               <Flex justifyContent="center" borderRadius="md" border="1px" borderColor="#dcdcdc" width="450px" height="450px" marginTop="30px" backgroundColor="white">
                 <Box width="350px" height="100%">
-                  <FormLabel id="userName" fontSize="20px" fontWeight="bold" marginTop="30px">ユーザー名 or メールアドレス</FormLabel>
-                  <Input id='userName' backgroundColor="white"></Input>
+                  <FormLabel id="maladdress" fontSize="20px" fontWeight="bold" marginTop="30px">メールアドレス</FormLabel>
+                  <Input id='mailaddress' backgroundColor="white"></Input>
                   <FormLabel id="pass" fontSize="20px" fontWeight="bold" marginTop="30px">パスワード</FormLabel>
                   <Input id='pass' backgroundColor="white"></Input>
                   <Button width="100%" height="50px" colorScheme='blue' marginTop="80px" marginBottom="20px">ログイン</Button>
